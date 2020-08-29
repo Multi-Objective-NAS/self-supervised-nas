260f94a828cbf15e4c49f3a5abf24568b1eb58f9
Minyoung Choe <chocindy05@gmail.com>
2020-08-29 21:08:28 +0900

    apply feedback

--- src/datasets.py
+++ src/datasets.py
@@ -5,39 +5,40 @@ import torch
 import numpy as np
 from hydra import utils as hydra_utils
 
-#import nasbench
-#import NAS-Bench-201
 from libs.nasbench201 import nas_201_api as api201
 from libs.nasbench.nasbench import api as api101
 from libs.nasbench.nasbench.lib import graph_util
-from libs.SemiNAS_modified.nasbench import utils as seminas_utils
+from libs.SemiNAS.nasbench import utils as seminas_utils
 
 
-def get_dataset(name, **kwargs):
-    # nasbench.api.NASBench
-    
-    if name == 'nasbench101' or name == 'nasbench201':
-        return NASBench(name, **kwargs)
-    else:
-        return None
+def get_dataset(name, path, **kwargs):
+    assert name == 'nasbench101' or name == 'nasbench201'
 
-
-class NASBench(torch.utils.data.IterableDataset):
-    def __init__(self, name, path, samples_per_class):
-        if not pathlib.Path(path).is_absolute():
+    if not pathlib.Path(path).is_absolute():
             path = hydra_utils.to_absolute_path(path)
         assert pathlib.Path(path).exists()
+
+    if name == 'nasbench101':
+        return NASBench(engine=api101.NASBench(path), **kwargs)
+    elif name == 'nasbench201':
+        return NASBench(engine=api201.NASBench201API(path), **kwargs)
+
+class NASBench(torch.utils.data.IterableDataset):
+    def __init__(self, engine, samples_per_class):
         
-        self.engine = None
-        if name == 'nasbench101':
-            self.engine = api101.NASBench(path)
-        elif name == 'nasbench201':
-            self.engine = api201.NASBench201API(path)
+        self.engine = engine
             
         self.samples_per_class = samples_per_class
         self.name = name
-        self.len = 0
-        self.search_space = self.engine.search_space
+        self.search_space = engine.search_space # list of possible operations ex.[ CONV 3x3, MAXPOOL 3x3, ... ]
+
+        length = 0
+        for index, key in enumerate(engine.hash_iterator()):
+                arch = engine.get_model_spec_by_hash(key)
+                matrix, ops = arch.matrix, arch.ops
+                if matrix.shape[0] == 7:
+                    length += 1
+        self._dataset_length = length
 
     def __iter__(self):
         for index, matrix, ops in self._random_graph_generator():
@@ -45,12 +46,6 @@ class NASBench(torch.utils.data.IterableDataset):
                 yield self._encode(pmatrix, pops), index
 
     def __len__(self):
-        if self.len == 0:
-            for index, key in enumerate(self.engine.hash_iterator()):
-                arch = self.engine.get_model_spec_by_hash(key)
-                matrix, ops = arch.matrix, arch.ops
-                if matrix.shape[0] == 7:
-                    self.len += 1
         return self.len * self.samples_per_class
 
     def _random_graph_generator(self):
