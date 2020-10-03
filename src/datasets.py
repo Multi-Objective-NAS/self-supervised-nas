ab81d875c053ce16f3c99d1c57fcb323fc292cf9
Kwanghee Choi <juice500ml@gmail.com>
2020-10-04 04:51:51 +0900

    Add zero-padding (#23)
    * Multiple modifications:
    
    - Remove edit_distance_three case (Thanks to @young917)
    - Add GeneralizedLiftedStructureLoss
    - Remove len(matrix) == 7 limitation by zeropadding
    
    * Modify parameters for nasbench201 controller
    
    * Add max_seq_len to config and add config validation check
    
    * get_metrics_from_hash -> get_modelspec_by_hash
    
    * get_metrics_from_hash -> get_modelspec_by_hash
    
    * Add decode for dataset, graph_modifier can now change matrix dimension
    
    * filter invalid model output
    
    * Update SemiNAS library
    
    * Clean up nasbench201 settings
    
    Co-authored-by: Minyoung Choe <chocindy05@gmail.com>

--- src/datasets.py
+++ src/datasets.py
@@ -3,6 +3,7 @@ import pathlib
 from hydra import utils as hydra_utils
 import numpy as np
 import torch
+import math
 
 from nasbench import api as api101
 from nasbench.lib import graph_util
@@ -33,34 +34,36 @@ def get_dataset(name, path, mode, **kwargs):
         dataset_cls = PretrainNASBench
     elif mode == 'train':
         dataset_cls = TrainNASBench
-    return dataset_cls(engine=engine, model_spec=model_spec, **kwargs)
+    return dataset_cls(
+        engine=engine,
+        model_spec=model_spec,
+        **kwargs
+    )
 
 
 class PretrainNASBench(torch.utils.data.IterableDataset):
-    def __init__(self, engine, model_spec, samples_per_class, graph_modify_ratio):
+    def __init__(self, engine, model_spec, max_seq_len, samples_per_class, graph_modify_ratio):
         self.engine = engine
         self.model_spec = model_spec
+        self.max_seq_len = max_seq_len
         self.samples_per_class = samples_per_class
 
         # list of possible operations ex.[ CONV 3x3, MAXPOOL 3x3, ... ]
         self.search_space = engine.search_space
 
         # Find dataset length
-        length = 0
-        for key in engine.hash_iterator():
-            arch = engine.get_modelspec_by_hash(key)
-            if arch.matrix.shape[0] == 7:
-                length += 1
-        self._dataset_length = length
+        self._dataset_length = len(engine.hash_iterator())
 
         self.graph_modifier = GraphModifier(
             validate=self.is_valid,
             operations=set(self.search_space),
-            samples_per_class=samples_per_class,
+            samples_per_class=samples_per_class - 1,
             **graph_modify_ratio)
 
     def __iter__(self):
         for index, matrix, ops in self._random_graph_generator():
+            seq = self._encode(matrix, ops)
+            yield (seq, [0] + seq[:-1]), index
             for pmatrix, pops in self.graph_modifier.generate_modified_models(matrix, ops):
                 seq = self._encode(pmatrix, pops)
                 yield (seq, [0] + seq[:-1]), index
@@ -72,11 +75,13 @@ class PretrainNASBench(torch.utils.data.IterableDataset):
         for index, key in enumerate(self.engine.hash_iterator()):
             arch = self.engine.get_modelspec_by_hash(key)
             matrix, ops = arch.matrix, arch.ops
-            if matrix.shape[0] == 7:
-                yield (index, matrix, ops)
+            yield (index, matrix, ops)
 
     def _encode(self, matrix, ops):
-        return seminas_utils.convert_arch_to_seq(matrix, ops, self.search_space)
+        seq = seminas_utils.convert_arch_to_seq(matrix, ops, self.search_space)
+        seq += [0] * (self.max_seq_len - len(seq))
+        assert len(seq) == self.max_seq_len
+        return seq
 
     def is_valid(self, matrix, ops):
         model = self.model_spec(matrix=matrix, ops=ops)
@@ -90,18 +95,15 @@ class PretrainNASBench(torch.utils.data.IterableDataset):
 
 
 class TrainNASBench(torch.utils.data.Dataset):
-    def __init__(self, engine, model_spec, batch_size, writer):
+    def __init__(self, engine, model_spec, max_seq_len, batch_size, writer):
         self.engine = engine
         self.model_spec = model_spec
+        self.max_seq_len = max_seq_len
         self.batch_size = batch_size
         self.writer = writer
 
         self.dataset = []
         self.seqs = []
-
-    def _query(self, matrix, ops):
-        arch = self.model_spec(matrix=matrix, ops=ops)
-        return self.engine.query(arch, 'valid'), self.engine.query(arch, 'test')
 
     def _append(self, seq, sampled_perf, true_perf):
         self.seqs.append(seq)
@@ -116,34 +118,66 @@ class TrainNASBench(torch.utils.data.Dataset):
         self.writer.add_scalar(
             f'Metric/true_performance', true_perf, len(self.dataset))
 
+    def _encode(self, matrix, ops):
+        seq = seminas_utils.convert_arch_to_seq(matrix, ops, self.engine.search_space)
+        seq += [0] * (self.max_seq_len - len(seq))
+        assert len(seq) == self.max_seq_len
+        return seq
+
+    def _decode(self, seq):
+        assert 6 in seq
+        seq = seq[:seq.index(6) + 1]
+
+        n = int(math.floor(math.sqrt((len(seq) + 1) * 2)))
+        assert n >= 2
+        assert len(seq) == (n + 2) * (n - 1) / 2
+
+        for i in range(n - 1):
+            offset = (i + 3) * i // 2
+            for j in range(i + 1):
+                assert 1 <= seq[offset + j] <= 2
+            idx = seq[offset + i + 1] - 3
+            assert 0 <= idx <= len(self.engine.search_space)
+
+        matrix, ops = seminas_utils.convert_seq_to_arch(seq, self.engine.search_space)
+        arch = self.model_spec(matrix=matrix, ops=ops)
+        return arch
+
+    def _query(self, arch):
+        return self.engine.query(arch, 'valid'), self.engine.query(arch, 'test')
+
     def prepare(self, count):
         for key in self.engine.hash_iterator():
-            fixed_stat, _ = self.engine.get_metrics_from_hash(key)
-            matrix, ops = np.array(fixed_stat['module_adjacency']), fixed_stat['module_operations']
-            if matrix.shape[0] == 7:
-                sampled_perf, true_perf = self._query(matrix, ops)
-                self._append(
-                    seq=seminas_utils.convert_arch_to_seq(matrix, ops, self.engine.search_space),
-                    sampled_perf=sampled_perf,
-                    true_perf=true_perf,
-                )
-                if len(self.dataset) >= count:
-                    break
+            arch = self.engine.get_modelspec_by_hash(key)
+            sampled_perf, true_perf = self._query(arch)
+            self._append(
+                seq=self._encode(arch.matrix, arch.ops),
+                sampled_perf=sampled_perf,
+                true_perf=true_perf,
+            )
+            if len(self.dataset) >= count:
+                break
 
     def add(self, seqs):
         for seq in seqs:
-            matrix, ops = seminas_utils.convert_seq_to_arch(seq, self.engine.search_space)
-            sampled_perf, true_perf = self._query(matrix, ops)
+            arch = self._decode(seq)
+            sampled_perf, true_perf = self._query(arch)
             self._append(
-                seq=seminas_utils.convert_arch_to_seq(matrix, ops, self.engine.search_space),
+                seq=self._encode(arch.matrix, arch.ops),
                 sampled_perf=sampled_perf,
                 true_perf=true_perf,
             )
 
     def is_valid(self, seq):
-        matrix, ops = seminas_utils.convert_seq_to_arch(seq, self.engine.search_space)
-        arch = self.model_spec(matrix=matrix, ops=ops)
-        return self.engine.is_valid(arch) and len(arch.ops) == 7 and seq not in self.seqs
+        try:
+            arch = self._decode(seq)
+        except AssertionError:
+            return False
+        if self.engine.is_valid(arch):
+            # arch.matrix is None if arch is not valid.
+            return self._encode(arch.matrix, arch.ops) not in self.seqs
+        else:
+            return False
 
     def shuffled(self):
         return torch.utils.data.DataLoader(
