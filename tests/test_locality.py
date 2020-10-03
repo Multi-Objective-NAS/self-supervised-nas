fcdc407d7c3ad5d98999d23208ee85b88ec5da90
Naruu <esara2021@gmail.com>
2020-10-03 09:46:08 +0900

    Add multiprocessing pool

--- tests/test_locality.py
+++ tests/test_locality.py
@@ -1,10 +1,11 @@
 import argparse
-import random
+import csv
+import datetime
+from multiprocessing import Pool
+import logging
 import os
+import random
 
-import tqdm
-import pandas as pd
-import matplotlib.pyplot as plt
 import networkx as nx
 
 from nasbench import api as api101
@@ -17,7 +18,9 @@ RANDOM_SEED = 100
 SEED_ARCH_COUNT = 1000
 MAX_EDIT_DISTANCE = 4
 OUTPUT_PATH = "outputs_test_acc"
-do_not_save_plot = False
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s %(levelname)s %(processName)s %(message)s")
 
 
 def get_edit_distance(matrix1, ops1, matrix2, ops2, upper_bound):
@@ -36,24 +39,33 @@ def get_edit_distance(matrix1, ops1, matrix2, ops2, upper_bound):
     G1 = to_nx_graph(matrix1, ops1)
     G2 = to_nx_graph(matrix2, ops2)
 
-    return nx.graph_edit_distance(G1, G2, node_match=node_match, edge_match=edge_match, upper_bound=upper_bound) or -1
+    return nx.graph_edit_distance(G1, G2, node_match=node_match, edge_match=edge_match, upper_bound=upper_bound) or 0
 
 
-# TODO: change plot type to cdf?
-def save_plot(df, output_path):
-    max_edit_distance = int(df["edit_distance"].max())
-    fig, axes = plt.subplots(1, max_edit_distance, sharey=True, figsize=(4, 6))
-    fig.suptitle('Distribution of test_accuracy difference by edit-distance')
-    axes[0].set_ylabel("test accuracy difference")
-    plt.gca().invert_yaxis()
+def single_lookup(seed_hash, dataset, output_path):
+    logging.info(f"START: seed_hash: {seed_hash}")
+    seed_arch = dataset.get_modelspec_by_hash(seed_hash)
+    seed_matrix, seed_ops = seed_arch.matrix, seed_arch.ops
+    seed_test_acc = dataset.query(
+        api101.ModelSpec(matrix=seed_matrix, ops=seed_ops), 'test', epochs=108)
 
-    for ed, ax in zip(range(1, max_edit_distance + 1), axes):
-        diffs = df.loc[df["edit_distance"] == ed, "test_difference"]
-        ax.hist(diffs, orientation="horizontal")
-        ax.set_xlabel(f'edit-distance={ed}')
+    row_dict = dict()
+    for _, arch_hash in enumerate(dataset.hash_iterator()):
+        arch = dataset.get_modelspec_by_hash(arch_hash)
+        matrix, ops = arch.matrix, arch.ops
+        arch_test_acc = dataset.query(
+            api101.ModelSpec(matrix=matrix, ops=ops), 'test', epochs=108)
 
-    plt.savefig(f"{output_path}/figure_distribution.png")
-    plt.close()
+        true_distance = get_edit_distance(seed_matrix, seed_ops, matrix, ops, max_edit_distance)
+        row_dict[true_distance] = ((true_distance, seed_hash, arch_hash, abs(seed_test_acc - arch_test_acc), seed_test_acc, arch_test_acc))
+
+        # row_dict.keys() : 0, 1, ..., max_edit_distance
+        if len(row_dict) == max_edit_distance + 1:
+            break
+
+    with open(f'{output_path}/{seed_hash}.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(list(row_dict.values()))
 
 
 if __name__ == '__main__':
@@ -65,46 +77,18 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--dataset_path', required=True, help='path to dataset')
     parser.add_argument('-o', '--output_path', required=False, default=OUTPUT_PATH,
                         help=f'path to output directory, default "{OUTPUT_PATH}"')
-    parser.add_argument('-ns', '--no-save-plot', dest='do_not_save_plot', action='store_true',required=False, 
-                        help='do not save plot as images. default: save plot on given "output_path"')
     args = parser.parse_args()
 
     seed_arch_count = args.seed_arch_count
     max_edit_distance = args.max_edit_distance
     output_path = args.output_path
-    do_not_save_plot = args.do_not_save_plot
 
     random.seed(RANDOM_SEED)
     dataset = api101.NASBench(args.dataset_path)
 
-    rows = []
-    for idx, seed_hash in enumerate(tqdm.tqdm(random.sample(dataset.hash_iterator(), seed_arch_count))):
-        seed_arch = dataset.get_modelspec_by_hash(seed_hash)
-        seed_matrix, seed_ops = seed_arch.matrix, seed_arch.ops
-        seed_test_acc = dataset.query(
-            api101.ModelSpec(matrix=seed_matrix, ops=seed_ops), 'test', epochs=108)
-
-        row_dict = dict()
-        for _, arch_hash in enumerate(dataset.hash_iterator()):
-            arch = dataset.get_modelspec_by_hash(arch_hash)
-            matrix, ops = arch.matrix, arch.ops
-            arch_test_acc = dataset.query(
-                api101.ModelSpec(matrix=matrix, ops=ops), 'test', epochs=108)
-
-            with Pool(12) as p:
-                rows = p.map(, zip(graphs_1, graphs_2))
-
-            true_distance = get_edit_distance(seed_matrix, seed_ops, matrix, ops, max_edit_distance)
-            row_dict[true_distance] = ((true_distance, seed_hash, arch_hash, abs(seed_test_acc - arch_test_acc), seed_test_acc, arch_test_acc))
-            # row_dict.keys() : 0, 1, ..., max_edit_distance
-            if len(row_dict) == max_edit_distance + 1:
-                rows.extend(list(row_dict.values()))
-                break
-
-    columns = ['edit_distance', 'seed_hash', 'arch_hash', 'test_difference', 'seed_test_accuracy', 'arch_test_accuracy']
-    df = pd.DataFrame(rows, columns=columns)
-
+    output_path = f"{output_path}/csv-{datetime.datetime.now().strftime('%m-%d-%H-%M')}"
     os.makedirs(output_path, exist_ok=True)
-    df.to_csv(f'{output_path}/distribution.csv', index=False)
-    if not do_not_save_plot:
-        save_plot(df, output_path)
+
+    seed_hashes = random.sample(dataset.hash_iterator(), seed_arch_count)
+    with Pool(8) as p:
+        p.starmap(single_lookup, [(seed_hash, dataset, output_path) for seed_hash in seed_hashes])
