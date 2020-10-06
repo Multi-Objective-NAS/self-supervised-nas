8fec30d3068be9971e40c890d15cf15129dee8de
Naruu <esara2021@gmail.com>
2020-10-06 13:01:05 +0900

    Remove random seed, add random.shuffle

--- tests/locality_test.py
+++ tests/locality_test.py
@@ -13,10 +13,8 @@ from nasbench import api as api101
 
 NASBENCH_101_DATASET = "/home/dzzp/workspace/dataset/nasbench_only108.tfrecord"
 
-RANDOM_SEED = 100
-
-SEED_ARCH_COUNT = 1000
-MAX_EDIT_DISTANCE = 4
+SEED_ARCH_COUNT = 300
+MAX_EDIT_DISTANCE = 6
 OUTPUT_PATH = "outputs_test_acc"
 
 logging.basicConfig(level=logging.INFO,
@@ -50,14 +48,16 @@ def single_lookup(seed_hash, dataset, output_path):
         api101.ModelSpec(matrix=seed_matrix, ops=seed_ops), 'test', epochs=108)
 
     row_dict = dict()
-    for _, arch_hash in enumerate(dataset.hash_iterator()):
+    hash_iterator = list(dataset.hash_iterator())
+    random.shuffle(hash_iterator)
+    for _, arch_hash in enumerate(hash_iterator):
         arch = dataset.get_modelspec_by_hash(arch_hash)
         matrix, ops = arch.matrix, arch.ops
         arch_test_acc = dataset.query(
             api101.ModelSpec(matrix=matrix, ops=ops), 'test', epochs=108)
 
         true_distance = get_edit_distance(seed_matrix, seed_ops, matrix, ops, max_edit_distance)
-        row_dict[true_distance] = ((true_distance, seed_hash, arch_hash, abs(seed_test_acc - arch_test_acc), seed_test_acc, arch_test_acc))
+        row_dict[true_distance] = ((true_distance, seed_hash, arch_hash, seed_test_acc - arch_test_acc, seed_test_acc, arch_test_acc))
 
         # row_dict.keys() : 0, 1, ..., max_edit_distance
         if len(row_dict) == max_edit_distance + 1:
@@ -72,9 +72,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--seed_arch_count', type=int, required=False, default=SEED_ARCH_COUNT,
                         help=f'number_of_seed_architectures. default: {SEED_ARCH_COUNT}')
-    parser.add_argument('-ed', '--max_edit_distance', type=int, default=MAX_EDIT_DISTANCE,
+    parser.add_argument('-ed', '--max_edit_distance', type=int, default=MAX_EDIT_DISTANCE, required=False,
                         help=f'max edit distance to look up from dataset. default: {MAX_EDIT_DISTANCE}')
-    parser.add_argument('-i', '--dataset_path', required=True, help='path to dataset')
+    parser.add_argument('-i', '--dataset_path', required=False, default=NASBENCH_101_DATASET, help='path to dataset')
     parser.add_argument('-o', '--output_path', required=False, default=OUTPUT_PATH,
                         help=f'path to output directory, default "{OUTPUT_PATH}"')
     args = parser.parse_args()
@@ -83,7 +83,6 @@ if __name__ == '__main__':
     max_edit_distance = args.max_edit_distance
     output_path = args.output_path
 
-    random.seed(RANDOM_SEED)
     dataset = api101.NASBench(args.dataset_path)
 
     output_path = f"{output_path}/csv-{datetime.datetime.now().strftime('%m-%d-%H-%M')}"
